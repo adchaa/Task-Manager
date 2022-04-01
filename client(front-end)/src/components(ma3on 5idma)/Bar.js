4b60e6f6b269d1839ad2eed231e8f1528cda2b3f
adem <adem0946chaa@gmail.com>
2022-04-01 15:20:04 +0100

    .

--- client(front-end)/src/components(ma3on 5idma)/Bar.js
+++ client(front-end)/src/components(ma3on 5idma)/Bar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Bar = () => {
   return (
     <div className="bar">
-      <img src={logo} alt="logo"/>
+      <img className="logo" src={logo} alt="logo"/>
       <div className="logsign">
         <Link to="/login" style={{ textDecoration: 'none' }}><p className="login"> Login</p></Link>
         <Link to="/signup" style={{ textDecoration: 'none' }}><p className="signup">Sign Up</p></Link>
