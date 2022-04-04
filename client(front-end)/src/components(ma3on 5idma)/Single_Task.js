6614679e082dcd554a3da26440e9fe9f11cc118a
adem <adem0946chaa@gmail.com>
2022-04-04 11:18:56 +0100

    started dev task page

---
+++ client(front-end)/src/components(ma3on 5idma)/Single_Task.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Single_Task({ task_text, task_desc }) {
+  return (
+    <div className="task_box">
+      <h4>{task_text}</h4>
+      <p>{task_desc}</p>
+    </div>
+  );
+}
+
+export default Single_Task;
